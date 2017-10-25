6787985936b5298ce811ac307347d4ffd93cdb3b
kokubu-yumiko <kokubu-yumiko@kayac.com>
2017-10-25 15:38:04 +0900

    add Procfile

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
